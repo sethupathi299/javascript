6e4a987a6cd531d3830be12c5eb816cd8030afe4
sethupathi299 <54588533+sethupathi299@users.noreply.github.com>
2019-08-29 20:52:53 +0530

    Delete array.js

--- array.js
+++
@@ -1,6 +0,0 @@
-var house = ["1BHK", 25000, "2BHK", 50000, "Rent", true]; 
-console.log(house[0]+" cost= "+house[1]); 
-var cost_1BHK = house[1]; 
-var  = house[5]; 
-console.log("Cost of 1BHK = "+ cost 1BHK); 
-console.log("Is house for rent = ")+ is for rent); 
